a8a80749e46e574e5b5c8b1e8c77b001beff55b2
ikien.dang@gmail.com <ikien.dang@gmail.com>
2020-03-25 15:58:52 -0400

    Creating react app with palette section only, original palette colours added

---
+++ src/ColorBox.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./ColorBox.css";
+
+export default class ColorBox extends Component {
+  render() {
+    return (
+      <div style={{ background: this.props.background }} className="ColorBox">
+        <span>{this.props.name}</span>
+        <span>MORE</span>
+      </div>
+    );
+  }
+}
---
+++ src/Palette.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import ColorBox from "./ColorBox.js";
+import "./Palette.css";
+
+export default class Palette extends Component {
+  render() {
+    const colorBoxes = this.props.colors.map(color => (
+      <ColorBox background={color.color} name={color.name} />
+    ));
+    return (
+      <div className="Palette">
+        <div className="Palette-colors">{colorBoxes}</div>
+      </div>
+    );
+  }
+}
