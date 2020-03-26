4f5977e29f409e3d5401897c25178c2df17cfa57
ikien.dang@gmail.com <ikien.dang@gmail.com>
2020-03-26 12:58:13 -0400

    generating different shades of colors using chromajs

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from "react";
 import "./App.css";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
 
 function App() {
+  console.log(generatePalette(seedColors[4]));
   return (
     <div>
       <Palette {...seedColors[4]} />
