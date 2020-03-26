b719a358e7aae58951285ed15c394dd8f9f37715
ikien.dang@gmail.com <ikien.dang@gmail.com>
2020-03-26 15:14:40 -0400

    styling slider

--- src/Palette.js
+++ src/Palette.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox.js";
-import "./Palette.css";
+
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
+
+import "./Palette.css";
 
 // We can just import Slider or Range to reduce bundle size
 // import Slider from 'rc-slider/lib/Slider';
@@ -28,13 +30,16 @@ export default class Palette extends Component {
     ));
     return (
       <div className="Palette">
-        <Slider
-          defaultValue={level}
-          min={100}
-          max={900}
-          step={100}
-          onAfterChange={this.changeLevel}
-        />
+        <div className="slider">
+          <Slider
+            defaultValue={level}
+            min={100}
+            max={900}
+            step={100}
+            onAfterChange={this.changeLevel}
+          />
+        </div>
+
         <div className="Palette-colors">{colorBoxes}</div>
       </div>
     );
