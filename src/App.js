7a64212ae20b87dd926ee902a546e25d51923fec
ikien.dang@gmail.com <ikien.dang@gmail.com>
2020-03-27 12:56:22 -0400

    installing react router and initializing new routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
@@ -7,9 +8,14 @@ import { generatePalette } from "./colorHelpers";
 function App() {
   console.log(generatePalette(seedColors[4]));
   return (
-    <div>
-      <Palette palette={generatePalette(seedColors[4])} />
-    </div>
+    <Switch>
+      <Route exact path="/" render={() => <h1>Palette list goes here</h1>} />
+      <Route
+        exact
+        path="/palette/:id"
+        render={() => <h1>Individual Palette</h1>}
+      />
+    </Switch>
   );
 }
 
