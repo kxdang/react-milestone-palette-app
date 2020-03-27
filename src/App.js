0f463a54eb7ba4b5433e5e2a0d050e970e4cccaa
ikien.dang@gmail.com <ikien.dang@gmail.com>
2020-03-27 15:26:32 -0400

    working palette route based on id

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 
-function App() {
-  console.log(generatePalette(seedColors[4]));
-  return (
-    <Switch>
-      <Route exact path="/" render={() => <h1>Palette list goes here</h1>} />
-      <Route
-        exact
-        path="/palette/:id"
-        render={() => <h1>Individual Palette</h1>}
-      />
-    </Switch>
-  );
+class App extends Component {
+  findPalette(id) {
+    return seedColors.find(function(palette) {
+      return palette.id === id;
+    });
+  }
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" render={() => <h1>Palette list goes here</h1>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={routeProps => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
+      </Switch>
+    );
+  }
 }
 
 export default App;
