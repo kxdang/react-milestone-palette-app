3a164b7b7bd30e82548ff8856443242cd692520b
ikien.dang@gmail.com <ikien.dang@gmail.com>
2020-03-28 09:54:13 -0400

    initializing the styles for minipalette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -2,25 +2,46 @@ import React from "react";
 import { withStyles } from "@material-ui/styles";
 
 const styles = {
-  main: {
-    backgroundColor: "purple",
-    border: "3px solid teal",
-    "& h1": {
-      color: "white"
+  root: {
+    backgroundColor: "white",
+    borderRadius: "5px",
+    border: "1px solid black",
+    padding: "0.5rem",
+    position: "relative",
+    overflow: "hidden",
+
+    "&:hover": {
+      cursor: "pointer"
     }
   },
-  secondary: {
-    backgroundColor: "red"
+  colors: {
+    backgroundColor: "grey"
+  },
+  title: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    margin: "0",
+    color: "black",
+    paddingTop: "0.5rem",
+    fontSize: "1rem",
+    position: "relative"
+  },
+  emoji: {
+    marginLeft: "0.5rem",
+    fontSize: "1.5rem"
   }
 };
 
 function MiniPalette(props) {
-  const { classes } = props;
+  const { classes, paletteName, emoji } = props;
   console.log(classes);
   return (
-    <div className={classes.main}>
-      <h1>Mini Palette</h1>
-      <div className="secondary">Hello world</div>
+    <div className={classes.root}>
+      <div className={classes.colors}></div>
+      <h5 className={classes.title}>
+        {paletteName} <span>{emoji}</span>
+      </h5>
     </div>
   );
 }
