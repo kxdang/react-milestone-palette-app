df0d22c0829b7900e783f0c6c53069d50ffac020
ikien.dang@gmail.com <ikien.dang@gmail.com>
2020-03-31 14:33:32 -0400

    adding color picker component and connecting color picker to change dynamically with button

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -11,8 +11,11 @@ import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import Button from "@material-ui/core/Button";
 
-const drawerWidth = 240;
+import { ChromePicker } from "react-color";
+
+const drawerWidth = 400;
 
 const styles = theme => ({
   root: {
@@ -72,9 +75,14 @@ const styles = theme => ({
 });
 
 class NewPaletteForm extends Component {
-  state = {
-    open: false
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: true,
+      currentColor: "teal"
+    };
+    this.updateCurrentColor = this.updateCurrentColor.bind(this);
+  }
 
   handleDrawerOpen = () => {
     this.setState({ open: true });
@@ -83,6 +91,10 @@ class NewPaletteForm extends Component {
   handleDrawerClose = () => {
     this.setState({ open: false });
   };
+
+  updateCurrentColor(newColor) {
+    this.setState({ currentColor: newColor.hex });
+  }
 
   render() {
     const { classes } = this.props;
@@ -126,6 +138,27 @@ class NewPaletteForm extends Component {
             </IconButton>
           </div>
           <Divider />
+          <Typography variant="h4">Design your palette</Typography>
+          <div>
+            <Button variant="contained" color="secondary">
+              Clear Palette
+            </Button>
+            <Button variant="contained" color="Primary">
+              Random Color
+            </Button>
+          </div>
+
+          <ChromePicker
+            color={this.state.currentColor}
+            onChangeComplete={this.updateCurrentColor}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            style={{ backgroundColor: this.state.currentColor }}
+          >
+            Add Color
+          </Button>
         </Drawer>
         <main
           className={classNames(classes.content, {
