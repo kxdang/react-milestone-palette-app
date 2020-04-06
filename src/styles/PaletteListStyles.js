492f684d9d393be8810d94a5d12360e9d1c9964d
ikien.dang@gmail.com <ikien.dang@gmail.com>
2020-04-06 15:53:00 -0400

    making palette list responsive

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,3 +1,5 @@
+import sizes from "./sizes";
+
 export default {
   root: {
     backgroundColor: "#097ddb",
@@ -13,6 +15,12 @@ export default {
     alignItems: "flex-start",
     flexDirection: "column",
     flexWrap: "wrap",
+    [sizes.down("xl")]: {
+      width: "80%",
+    },
+    [sizes.down("xl")]: {
+      width: "75%",
+    },
   },
   nav: {
     display: "flex",
@@ -29,6 +37,12 @@ export default {
     width: "100%",
     display: "grid",
     gridTemplateColumns: "repeat(3, 30%)",
-    gridGap: "5%",
+    gridGap: "1.5rem",
+    [sizes.down("md")]: {
+      gridTemplateColumns: "repeat(2, 50%)",
+    },
+    [sizes.down("xs")]: {
+      gridTemplateColumns: "repeat(1, 100%)",
+    },
   },
 };
